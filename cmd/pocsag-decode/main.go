8984b221f253125dc2fabc6f24f9f5e61f1cce24
Marcell Csendes <sqpp@proton.me>
2025-10-03 05:37:20 +0200

    Add command-line tools

---
+++ cmd/pocsag-decode/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	pocsag "github.com/marcell/pocsag-go"
+)
+
+func main() {
+	inputFile := flag.String("input", "", "Input WAV file to decode (required)")
+	flag.StringVar(inputFile, "i", "", "Input WAV file to decode (required) - short form")
+
+	flag.Parse()
+
+	if *inputFile == "" {
+		fmt.Fprintln(os.Stderr, "Error: Input file required")
+		fmt.Fprintln(os.Stderr, "\nUsage examples:")
+		fmt.Fprintln(os.Stderr, "  pocsag-decode --input message.wav")
+		fmt.Fprintln(os.Stderr, "  pocsag-decode -i message.wav")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Read WAV file
+	data, err := os.ReadFile(*inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Decode POCSAG
+	messages, err := pocsag.DecodeFromAudio(data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error decoding: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(messages) == 0 {
+		fmt.Println("No messages found")
+		return
+	}
+
+	// Display messages
+	fmt.Println("POCSAG1200: Decoded messages:")
+	for _, msg := range messages {
+		fmt.Println(msg.String())
+	}
+}
---
+++ cmd/pocsag/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	pocsag "github.com/marcell/pocsag-go"
+)
+
+func main() {
+	address := flag.Uint("address", 0, "Pager address (RIC) - REQUIRED")
+	flag.UintVar(address, "a", 0, "Pager address (RIC) - REQUIRED")
+
+	message := flag.String("message", "", "Message text to send - REQUIRED")
+	flag.StringVar(message, "m", "", "Message text to send - REQUIRED")
+
+	output := flag.String("output", "output.wav", "Output WAV file path")
+	flag.StringVar(output, "o", "output.wav", "Output WAV file path")
+
+	funcCode := flag.Uint("function", pocsag.FuncAlphanumeric, "Message type: 0=numeric, 3=alphanumeric (default: 3)")
+	flag.UintVar(funcCode, "f", pocsag.FuncAlphanumeric, "Message type: 0=numeric, 3=alphanumeric")
+
+	flag.Parse()
+
+	// Check required parameters
+	if *address == 0 || *message == "" {
+		fmt.Fprintln(os.Stderr, "Error: Address and message are required")
+		fmt.Fprintln(os.Stderr, "\nUsage examples:")
+		fmt.Fprintln(os.Stderr, "  # Alphanumeric message:")
+		fmt.Fprintln(os.Stderr, "  pocsag --address 123456 --message \"HELLO WORLD\" --output test.wav")
+		fmt.Fprintln(os.Stderr, "  pocsag -a 123456 -m \"HELLO WORLD\" -o test.wav")
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "  # Numeric message:")
+		fmt.Fprintln(os.Stderr, "  pocsag --address 999888 --message \"0123456789\" --function 0 --output num.wav")
+		fmt.Fprintln(os.Stderr, "  pocsag -a 999888 -m \"0123456789\" -f 0 -o num.wav")
+		fmt.Fprintln(os.Stderr, "")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Create POCSAG packet
+	packet := pocsag.CreatePOCSAGPacket(uint32(*address), *message, uint8(*funcCode))
+
+	// Convert to audio
+	wavData := pocsag.ConvertToAudio(packet)
+
+	// Write to file
+	err := os.WriteFile(*output, wavData, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("✅ Generated %s\n", *output)
+	fmt.Printf("   Address: %d, Function: %d, Message: %s\n", *address, *funcCode, *message)
+	fmt.Printf("\nTest with: multimon-ng -t wav -a POCSAG1200 %s\n", *output)
+}
